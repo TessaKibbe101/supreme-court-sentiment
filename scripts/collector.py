6216247c39d8aa5cd2a53da50d5c824d67b6745c
TessaKibbe101 <your-email@example.com>
2025-12-22 16:30:47 -0800

    Add sentiment analysis using NLTK VADER

--- scripts/collector.py
+++ scripts/collector.py
@@ -1,3 +1,5 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 def read_opinion_file(filepath):
     try:
         with open(filepath, 'r') as f:
@@ -12,6 +14,10 @@ def count_stats(text):
     character_count = len(text)
     return {'words': word_count, 'characters': character_count}
 
+def analyze_sentiment(text):
+    analyzer = SentimentIntensityAnalyzer()
+    score = analyzer.polarity_scores(text)
+    return score
 
 def main():
     opinion_file = read_opinion_file("/Users/tessakibbe/supreme-court-sentiment/data/raw_opinions/Harvard_2023.txt")
@@ -19,10 +25,15 @@ def main():
         return
 
     count = count_stats(opinion_file)
-
+    sentiment = analyze_sentiment(opinion_file)
     print("Case: Harvard Affirmative Action 2023")
     print(f"Words: {count['words']}")
-    print(f"Characters: {count['characters']}")
+    print(f"Characters: {count['characters']}\n")
+    print("Sentiment Analysis: ")
+    print(f"Positive: {sentiment['pos']}")
+    print(f"Negative: {sentiment['neg']}")
+    print(f"Neutral: {sentiment['neu']}")
+    print(f"Compound: {sentiment['compound']}\n")
     print(f"First 100 characters: {opinion_file[:100]}")
 
 if __name__ == "__main__":
